722b1497730f8a4f856764f5cb2dcfb3b2c73889
toesteatoes <ddyt904@gmail.com>
2023-10-20 11:03:21 -0700

    11 done 12 started

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let ageArray = arr.map((person) => person.yearOfDeath - person.yearOfBirth);
+  return arr[1].name; 
 };
 
 // Do not edit below this line
