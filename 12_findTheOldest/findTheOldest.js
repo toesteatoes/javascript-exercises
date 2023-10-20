2dc92275e9f8b2de6c14d201d590630ebe926fcb
toesteatoes <ddyt904@gmail.com>
2023-10-20 11:29:32 -0700

    npm for 12 is being dumb, basically done but no 9

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
 const findTheOldest = function(arr) {
-  let ageArray = arr.map((person) => person.yearOfDeath - person.yearOfBirth);
-  return arr[1].name; 
+	let ageArray = arr.map((person) => (person.yearOfDeath - person.yearOfBirth));
+  return arr[ageArray.indexOf(Math.max(...ageArray))].name;
+
+  //this works in sandbox, npm test brokn
 };
 
 // Do not edit below this line
